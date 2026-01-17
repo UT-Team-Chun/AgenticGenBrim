392a2fee6a16f30f44cf0ab48e1ef63d4b007687
Hiro1894 <hiroki18kapnooodle94@gmail.com>
2026-01-17 21:10:04 +0900

    fix: import

--- src/bridge_json_to_ifc/ifc_utils_new/core/__init__.py
+++ src/bridge_json_to_ifc/ifc_utils_new/core/__init__.py
@@ -1,5 +1,6 @@
 """core - コアロジック"""
 
-from .DefBridge import *
+# DefBridgeは循環インポートを避けるため、直接インポートで使用する
+# from src.bridge_json_to_ifc.ifc_utils_new.core.DefBridge import ...
 from .DefIFC import *
 from .DefMath import *
